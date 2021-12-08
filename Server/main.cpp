711a3c318b6dd864db7516937e3ba8013defd980
James Puleo <james@jame.xyz>
2021-12-08 07:58:02 -0500

    Server: Use `serenity_main` for the entry point

--- Server/main.cpp
+++ Server/main.cpp
@@ -6,6 +6,7 @@
 
 #include <LibCore/ArgsParser.h>
 #include <LibCore/File.h>
+#include <LibMain/Main.h>
 #include <LibTerraria/World.h>
 #include <Server/Server.h>
 
@@ -13,7 +14,7 @@ static Server* s_server;
 
 Server& server() { return *s_server; }
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     Core::ArgsParser args_parser;
 
@@ -21,19 +22,13 @@ int main(int argc, char** argv)
 
     args_parser.add_positional_argument(world_path, "Path to the world file", "world");
 
-    if (!args_parser.parse(argc, argv))
+    if (!args_parser.parse(arguments))
         return 1;
 
-    auto file_or_error = Core::File::open(world_path, Core::OpenMode::ReadOnly);
+    auto file = TRY(Core::File::open(world_path, Core::OpenMode::ReadOnly));
 
-    if (file_or_error.is_error())
-    {
-        warnln("Failed to open world file: {}", file_or_error.error());
-        return 2;
-    }
-
-    auto file_bytes = file_or_error.value()->read_all();
-    auto bytes_stream = InputMemoryStream(file_bytes);
+    auto file_bytes = file->read_all();
+    InputMemoryStream bytes_stream(file_bytes);
 
     auto world_or_error = Terraria::World::try_load_world(bytes_stream);
 
